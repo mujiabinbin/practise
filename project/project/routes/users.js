48c322c551ae450cfbf0c6308f46643e52a1eb60
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-02-10 00:36:59 +0800

    数据库登陆注册

--- project/project/routes/users.js
+++ project/project/routes/users.js
@@ -1,26 +1,68 @@
 var express = require('express');
 //后端路由，用于分配ajax请求
 var router = express.Router();
-const users = [{
+const {Schema, model}=require("mongoose");
+const userschema= new Schema({
+	username:String,
+	password:String,
+})
+//2 定义数据集合的模型,将结构schema和数据库的集合关联起来
+//model("模型名称"，userschema，“数据库中集合名称”)
+const usermodel= model("usermodel",userschema,"users");
+/* usermodel.create({
+	username:"zhangsan",
+	password:"123"
+},function(err,data){
+	if(!err){
+		console.log(`add success`)
+	}else{
+		console.log(err)
+	}
+}) */
+/* usermodel.find(function(err,data){
+	console.log(data);
+	//return data;
+}) */
+
+//console.log(users);
+/* const users = [{
 	username: "zhangsan",
 	password: "123"
 }, {
 	username: "lisi",
 	password: "123"
-}]
+}] */
 
 /* GET users listing. */
 /* router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 }); */
 router.post('/login', function(req, res, next) {
+	usermodel.find(function(err,data){
+		const result=data.some(function(item,index){
+			return item.username == req.body.username && item.password == req.body.password;
+		})
+		if (result) {
+			res.send({
+				message: "登陆成功",
+				status: 1
+			});
+		} else {
+			res.send({
+				message: "登陆失败",
+				status: 0
+			});
+		}
+	});
 	//console.log("success login")
 	//1 接收前端数据
 	//- post:req.body
 	//- get:req.query
-	const result = users.some(function(item, index) {
+	/* const result = users.some(function(item, index) {
 		return item.username == req.body.username && item.password == req.body.userpass;
-	});
+	}); */
+	/* var result=usermodel.find(req.body.username);
+	console.log(result);
 	if (result) {
 		res.send({
 			message: "登陆成功",
@@ -32,35 +74,47 @@ router.post('/login', function(req, res, next) {
 			status: 0
 		});
 	}
-
+ */
 	//后端处理结果响应给前端
 
 });
 router.post('/reg', function(req, res, next) {
-
+    usermodel.find({username:req.body.username},function(err,data){
+		if(data.length>0){
+			res.send({
+				message: "用户名重复",
+				status: 0
+			}); 
+		}else{
+			res.send({
+				message: "注册成功",
+				status: 1
+			});
+			usermodel.create({username:req.body.username,password:req.body.password},function(err,data){
+				/* if(!err){
+					console.log(`register success`)
+				}else{
+					console.log(err);
+				} */
+			});
+		};
+		});
+	})
 	/* const objstr=JSON.stringify(req.body);
 	const strobj=JSON.parse(objstr); */
 	//console.log(strobj);
-	const isexist = users.some(function(item, index) {
+	/* const isexist = users.some(function(item, index) {
 		return item.username == req.body.username
-	})
-	if (isexist) {
-		res.send({
-			message: "用户名重复",
-			status: 0
-		});
-	} else {
-		res.send({
-			message: "注册成功",
-			status: 1
-		});
-		users.push(req.body);
-	}
+	}) */
 
-	console.log(users);
+	//console.log(users);
 
 	//后端处理结果响应给前端
-
-});
+	
+	/* async function a(){
+		const p=await usermodel.find();
+		console.log(p);
+	}
+	a(); */
 
 module.exports = router;
