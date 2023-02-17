737b4f5e5f3f0d226d2234c2906252286b7bb6c0
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-02-17 11:07:55 +0800

    学生系统添加学生校验

--- project/project/service/stuService.js
+++ project/project/service/stuService.js
@@ -4,11 +4,26 @@ const {
 	del,
 	search,
 	edit,
-	stuupdate
+	stuupdate,
+	isexsit
 } = require(`../dao/studao`);
 module.exports.add = async function(stu) {
 	const data = await add(stu);
 	return data;
+}
+module.exports.isexsit = async function(username) {
+	const data = await isexsit(username);
+	if (data.length > 0) {
+		return {
+			status: 0,
+			msg: "该用户已存在"
+		}
+	} else {
+		return {
+			status: 1,
+			msg: "用户不存在"
+		}
+	}
 }
 module.exports.stu = async function() {
 	const data = await stu();
