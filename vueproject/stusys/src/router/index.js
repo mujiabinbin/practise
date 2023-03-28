c8b40e165718c3a5c214f9887c614b68c0630d73
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-03-28 21:45:50 +0800

    vue路由其他配置

--- vueproject/stusys/src/router/index.js
+++ vueproject/stusys/src/router/index.js
@@ -1,31 +1,43 @@
-import { createRouter, createWebHashHistory,createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from '../views/home/Home';
-import Login from '../views/login/Login';
 import Reg from '../views/reg/Reg';
+const login = () => import('../views/login/Login');//箭头函数，依然是懒加载
+const err = () => import('../views/err/Err');
 
 
 const routes = [
   {
-    //路由信息对象
-  path:'/',
-  component:Home
+    //域名重定向，浏览器访问url会被重定向到redirect地址
+    path: '/home',
+    redirect: '/'
   },
   {
-    path:'/login',
-    component:Login
-    },
-    {
-      path:'/reg',
-      component:Reg
-      },
+    //路由信息对象
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/login',
+    //路由懒加载，只有当浏览器url匹配到“/login”时才会加载，而import Home from '../views/home/Home'是只要index.js被引用时就会加载
+    component: login//() => import('../views/login/Login')
+  },
+  {
+    path: '/reg',
+    component: Reg
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: err
+  }
 
- 
+
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   //路由配置
-  routes:routes
+  routes: routes,
+  //mode:'history'
 })
 //main.js引入此方法
 export default router
