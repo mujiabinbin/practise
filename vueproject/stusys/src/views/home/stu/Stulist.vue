802bcbc8c338f10acdb7ca6f6241f369da47d4c3
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-03-29 11:39:30 +0800

    嵌套路由，学生列表获取

---
+++ vueproject/stusys/src/views/home/stu/Stulist.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>学生列表</h2>
+    <thead>
+        <tr>
+            <th>编号</th>
+             <th>姓名</th>
+              <th>年龄</th>
+               <th>性别</th>
+                <th>操作</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="item in stulist" :key="item._id">
+            <td>{{item._id}}</td>
+            <td>{{item.username}}</td>
+            <td>{{item.userage}}</td>
+            <td>{{item.usergender}}</td>
+            <td><button>编辑</button><button>删除</button></td>
+        </tr>
+    </tbody>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            stulist:[]
+        }
+    },
+mounted() {
+    this.getstulist();
+},
+methods: {
+    async getstulist(){
+        const data = await this.$api.stuapi.get();
+        this.stulist=data.data.data.data
+    },
+    
+},
+}
+</script>
+
+<style>
+
+</style>
