dccfce97e82ef8735c03cf7a4e77eb4432cb75c2
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-03-30 17:33:28 +0800

    修改学生，分页

--- vueproject/stusys/src/views/home/stu/Stuedit.vue
+++ vueproject/stusys/src/views/home/stu/Stuedit.vue
@@ -4,7 +4,7 @@
     <input type="text" v-model="userage">
     <input type="radio" v-model="usergender" value="男">男
     <input type="radio" v-model="usergender" value="女">女
-    <button>修改</button>
+    <button @click="updatestu">修改</button>
   </div>
 </template>
 
@@ -19,7 +19,6 @@ export default {
     },
     props:['id'],
 created() {
-    //console.log(this.$route.params)
     this.geteditstudata()
 },
 methods: {
@@ -28,6 +27,12 @@ methods: {
       this.username=data.data.data[0].username;
       this.userage=data.data.data[0].userage;
       this.usergender=data.data.data[0].usergender;
+    },
+    async updatestu(){
+      const data=  await this.$api.stuapi.updatestu({_id:this.id,username:this.username,userage:this.userage,usergender:this.usergender});
+      if(data.data.status==1){
+        alert("编辑成功")
+      }
     }
 },
 }
