3a07fe3170305eb03814af8966c2a0b3fbc1c957
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-04-06 23:24:04 +0800

    头像上传，学生列表增加专业，班级，头像

--- vueproject/stusys/src/views/home/stu/Stuadd.vue
+++ vueproject/stusys/src/views/home/stu/Stuadd.vue
@@ -9,11 +9,12 @@
       <option  v-for="item in subdata" :key="item._id"  :value="item._id">{{ item.subname }}</option>
     </select>
     <span>班级</span>
-    <select>
-      <option  v-for="item in cladata" :key="item" value="item">{{ item }}</option>
+    <select v-model="userclaname">
+      <option  v-for="item in cladata" :key="item" :value="item">{{ item }}</option>
     </select>
     <label>上传图片</label>
     <input type="file" @change="uploadimg($event)">
+    <img :src="picurl" alt="">
     <button @click="addstu">提交</button>
   </div>
 </template>
@@ -25,6 +26,10 @@ export default {
       username:'',
       userage:'',
       usergender:'',
+      usersubname:'',
+      userclaname:'',
+      picname:'',
+      picurl:'',
       subdata:[],
       cladata:[],
       subid:{}
@@ -35,13 +40,12 @@ export default {
   },
 methods: {
   async addstu(){
-   const res= await this.api.stuapi.addstu({username:this.username,userage:this.userage,usergender:this.usergender});
+   const res= await this.api.stuapi.addstu({username:this.username,userage:this.userage,usergender:this.usergender,usersubname:this.usersubname,userclaname:this.userclaname,picname:this.picname});
    if(res.status==200){
       alert(`添加成功`)
    }else{
      alert(`添加失败`)
    }
-   console.log(res);
   },
   async getsublist(){
         const data = await this.api.subapi.subget();
@@ -49,10 +53,10 @@ methods: {
     },
     async searchsub(subid){
         const data = await this.api.subapi.searchsub(subid);
-        this.cladata=data.data[0].claname
-        console.log(data.data);
+        this.cladata=data.data[0].claname;
+        this.usersubname=data.data[0].subname
     },
-  uploadimg(event){
+  async uploadimg(event){
     //通过event拿到图片信息
     const files=event.target.files;
     //将图片信息添加到表单对象
@@ -60,6 +64,9 @@ methods: {
     //表单对象添加图片信息，file是后端接收此图片需要的名称，由后端确定，files是获取图片的信息
     fd.append('file',files[0]);
     //请求axios将fd作为参数传给统一发送请求接口
+    const data=await this.api.stuapi.uploadpic(fd);
+    this.picurl=`http://localhost:3000/temp/${data.data.data}`;
+    this.picname=data.data.data
   }
 },
 }
--- vueproject/stusys/src/views/home/stu/Stulist.vue
+++ vueproject/stusys/src/views/home/stu/Stulist.vue
@@ -1,80 +1,100 @@
 <template>
-  <div>
-    <h2>学生列表</h2>
-    <thead>
-        <tr>
-            <th>编号</th>
-             <th>姓名</th>
-              <th>年龄</th>
-               <th>性别</th>
+    <div>
+        <h2>学生列表</h2>
+        <thead>
+            <tr>
+                <th>编号</th>
+                <th>姓名</th>
+                <th>年龄</th>
+                <th>性别</th>
+                <th>专业</th>
+                <th>班级</th>
+                <th>头像</th>
                 <th>操作</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr v-for="item in stulist" :key="item._id">
-            <td>{{item._id}}</td>
-            <td>{{item.username}}</td>
-            <td>{{item.userage}}</td>
-            <td>{{item.usergender=='男' ? item.usergender : item.usergender='女' }}</td>
-            <td><router-link tag="button" :to='"/home/stuedit/"+item._id'>编辑</router-link><button @click="delstu(item._id)">删除</button></td>
-        </tr>
-    </tbody>
-    <div>
-        <span>每页显示</span>
-        <select v-model="pagedata.pagesize">
-            <option selected value="3">3</option>
-             <option selected value="5">5</option>
-            <option value="10">10</option>
-        </select><span>条数据</span> <span>共{{total}}条数据</span><span>当前第{{pagedata.currentpage}}页，共{{pages}}页</span></div>
-    <div>
-        <button @click="pagedata.currentpage=1">首页</button> <button @click="pagedata.currentpage--">上一页</button> <button @click="pagedata.currentpage++">下一页</button> <button @click="pagedata.currentpage=pages">尾页</button>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in stulist" :key="item._id">
+                <td>{{ item._id }}</td>
+                <td>{{ item.username }}</td>
+                <td>{{ item.userage }}</td>
+                <td>
+                    {{
+                        item.usergender == "男" ? item.usergender : (item.usergender = "女")
+                    }}
+                </td>
+                <td>{{ item.usersubname ? item.usersubname : "暂无" }}</td>
+                <td>{{ item.userclaname ? item.userclaname : "暂无" }}</td>
+                <td>
+                    <img :src="`http://localhost:3000/img/${item.picname ? item.picname : 'default.jpg'
+                        }`" />
+                </td>
+                <td>
+                    <router-link tag="button" :to="'/home/stuedit/' + item._id">编辑</router-link><button
+                        @click="delstu(item._id)">删除</button>
+                </td>
+            </tr>
+        </tbody>
+        <div>
+            <span>每页显示</span>
+            <select v-model="pagedata.pagesize">
+                <option selected value="3">3</option>
+                <option selected value="5">5</option>
+                <option value="10">10</option>
+            </select><span>条数据</span> <span>共{{ total }}条数据</span><span>当前第{{ pagedata.currentpage }}页，共{{ pages }}页</span>
+        </div>
+        <div>
+            <button @click="pagedata.currentpage = 1">首页</button>
+            <button @click="pagedata.currentpage--">上一页</button>
+            <button @click="pagedata.currentpage++">下一页</button>
+            <button @click="pagedata.currentpage = pages">尾页</button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-   
     data() {
         return {
-            stulist:[],
-            pagedata:{
-                pagesize:3,
-                currentpage:1
+            stulist: [],
+            pagedata: {
+                pagesize: 3,
+                currentpage: 1,
             },
-            total:0,
-            pages:1
-        }
+            total: 0,
+            pages: 1,
+        };
     },
-mounted() {
-    this.getstulist();
-    console.log(this.$router);
-},
-watch:{
-pagedata:{
-    handler(){
-         this.getstulist();
+    mounted() {
+        this.getstulist();
     },
-    deep:true
-}
-},
-methods: {
-    async getstulist(){
-        const data = await this.api.stuapi.get(this.pagedata);
-        this.stulist=data.data.data.data;
-        this.total=data.data.data.total;
-        this.pages=data.data.data.pages;
+    watch: {
+        pagedata: {
+            handler() {
+                this.getstulist();
+            },
+            deep: true,
+        },
     },
-    async delstu(_id){
-        const data = await this.api.stuapi.delstu({_id:_id});
-        alert(data.data.msg);
-         this.getstulist();
-    }
-    
-},
-}
+    methods: {
+        async getstulist() {
+            const data = await this.api.stuapi.get(this.pagedata);
+            this.stulist = data.data.data.data;
+            this.total = data.data.data.total;
+            this.pages = data.data.data.pages;
+            console.log(data.data.data.data);
+        },
+        async delstu(_id) {
+            const data = await this.api.stuapi.delstu({ _id: _id });
+            alert(data.data.msg);
+            this.getstulist();
+        },
+    },
+};
 </script>
 
-<style>
-
+<style scoped>
+ img{
+    width: 50px;
+ }
 </style>
