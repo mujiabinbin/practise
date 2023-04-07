8f86a274cd96d9a7ba8c027fbc3266632a51f3a9
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-04-07 11:52:37 +0800

    getters,actions,辅助函数

--- vueproject/stusys/src/store/index.js
+++ vueproject/stusys/src/store/index.js
@@ -1,11 +1,16 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import api from '../http/api'
 
 //创建仓库实例对象
 export default createStore({
   state: {
     num:0,
+    cladata:[]
   },
   getters: {
+    sum(state){
+     return state.num+10
+    }
   },
   mutations: {
     INCRECEMENT(state){
@@ -16,10 +21,23 @@ export default createStore({
    },
    INPUTNUM(state,payload){
       state.num=payload
+   },
+   GETCLADATA(state,payload){
+      state.cladata=payload
    }
 
   },
   actions: {
+    async getclalistAsync(context){
+      const data = await api.claapi.claget();
+      //state.cladata=data.data;
+      context.commit('GETCLADATA',data.data)
+  },
+
+  async addclaAsync(context,payload){
+    const data = await api.claapi.addcla(payload);
+    return data
+}
   },
   modules: {
   }
