9e474de4abaf137dea6f109265173935e80cc047
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-04-21 11:08:23 +0800

    ele 用户登陆及token验证全部流程

--- elestusys/src/router/index.js
+++ elestusys/src/router/index.js
@@ -22,32 +22,22 @@ const routes = [
         if(data.status==1){
           next();
           store.commit('GETUSERINFO',data.data)
-        }/* else if(data.response.status==401){
+        }else{
           ElMessageBox.confirm(
-            '请重新登陆',
-            'token失效',
+            '服务器错误，请重新登陆',
+            '抱歉',
             {
               confirmButtonText: '好的',
-              cancelButtonText: '取消',
               type: 'warning',
             }
           )
             .then(() => {
               router.push('/login')
             })
-            .catch(() => {
-              ElMessage({
-                type: 'info',
-                message: '操作取消',
-              })
-            })
-        } */
-        
-        console.log(data);
+        }
         //1 if判断token有效，next进入目标页，并成功从后端获取用户信息保存到状态机主仓库供其他组件调用
         //1.1 引入仓库对象import store from ‘@/store/index。js’，而后store。commite（写好的方法，参数）来修改state数据
         //2 token无效或过期，返回401报错
-
       }else{
         ElMessageBox.confirm(
           '您还没登陆，请重新登陆',
