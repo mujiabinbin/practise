602783cef99dd00f7e568c896a77074e340c8a2c
mujiabinbin <39404220+mujiabinbin@users.noreply.github.com>
2023-04-24 11:35:57 +0800

    虚拟节点，响应式原理1

--- elestusys/src/components/charts/Stucharts.vue
+++ elestusys/src/components/charts/Stucharts.vue
@@ -5,23 +5,30 @@
 <script>
 import * as echarts from "echarts";
 export default {
+  data() {
+    return {
+      stugender:["男","女"],
+      stugenderdata:[20,10]
+
+    }
+  },
   mounted() {
     //const main=this.$refs.stuchartsref;
     var myChart = echarts.init(document.querySelector(".charts"));
     myChart.setOption({
       title: {
-        text: "ECharts 入门示例",
+        text: "学生统计",
       },
       tooltip: {},
       xAxis: {
-        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        data: this.stugender,
       },
       yAxis: {},
       series: [
         {
-          name: "销量",
+          name: "数量",
           type: "bar",
-          data: [5, 20, 36, 10, 10, 20],
+          data: this.stugenderdata,
         },
       ],
     });
